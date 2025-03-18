8712f2f782b951e4b558bdcf9d2d7f3a744e9bb4
Diogo Barros <finkry@gmail.com>
2025-03-18 10:56:34 -0300

    Update server.js for web socket handling

--- result/server.js
+++ result/server.js
@@ -5,14 +5,14 @@ var express = require('express'),
     path = require('path'),
     app = express(),
     server = require('http').Server(app),
-    io = require('socket.io')(server);
+    io = require('socket.io')(server, { path: '/socket.io' });
 
 var port = process.env.PORT || 4000;
 
+// Socket.io connection handling
 io.on('connection', function (socket) {
-
   socket.emit('message', { text : 'Welcome!' });
-
+  
   socket.on('subscribe', function (data) {
     socket.join(data.channel);
   });
@@ -27,14 +27,14 @@ var pgDatabase = process.env.PG_DATABASE || 'postgres';
 
 var connectionString = `postgresql://${pgUser}:${pgPassword}@${pgHost}:${pgPort}/${pgDatabase}`;
 
-console.log(connectionString)
+console.log(connectionString);
 
 var pool = new Pool({
   connectionString: connectionString
 });
 
 async.retry(
-  {times: 1000, interval: 1000},
+  { times: 1000, interval: 1000 },
   function(callback) {
     pool.connect(function(err, client, done) {
       if (err) {
@@ -61,12 +61,12 @@ function getVotes(client) {
       io.sockets.emit("scores", JSON.stringify(votes));
     }
 
-    setTimeout(function() {getVotes(client) }, 1000);
+    setTimeout(function() { getVotes(client); }, 1000);
   });
 }
 
 function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
+  var votes = { a: 0, b: 0 };
 
   result.rows.forEach(function (row) {
     votes[row.vote] = parseInt(row.count);
@@ -77,14 +77,16 @@ function collectVotesFromResult(result) {
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/views'));
-app.use("/result", express.static(__dirname + '/views'));
 
+// Serve static files from the "views" folder for both routes
+app.use(express.static(path.join(__dirname, 'views')));
+app.use("/result", express.static(path.join(__dirname, 'views')));
+
+// Serve index.html for both "/" and "/result"
 app.get(['/', '/result'], function (req, res) {
   res.sendFile(path.resolve(__dirname, 'views', 'index.html'));
 });
 
 server.listen(port, function () {
-  var port = server.address().port;
-  console.log('App running on port ' + port);
+  console.log('App running on port ' + server.address().port);
 });
