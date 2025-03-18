0a3fbe89253ff3a6a3cc7fb28e2a9c31eac5e453
Diogo Barros <finkry@gmail.com>
2025-03-18 10:59:31 -0300

    Update app.js front-end

--- result/views/app.js
+++ result/views/app.js
@@ -1,50 +1,51 @@
 var app = angular.module('catsvsdogs', []);
-var socket = io.connect();
+
+// Use absolute connection so that it works on any route ("/" or "/result")
+var socket = io.connect(window.location.origin, { path: '/socket.io' });
 
 var bg1 = document.getElementById('background-stats-1');
 var bg2 = document.getElementById('background-stats-2');
 
-app.controller('statsCtrl', function($scope){
+app.controller('statsCtrl', function($scope) {
   $scope.aPercent = 50;
   $scope.bPercent = 50;
 
-  var updateScores = function(){
+  var updateScores = function() {
     socket.on('scores', function (json) {
-       data = JSON.parse(json);
-       var a = parseInt(data.a || 0);
-       var b = parseInt(data.b || 0);
+      var data = JSON.parse(json);
+      var a = parseInt(data.a || 0);
+      var b = parseInt(data.b || 0);
 
-       var percentages = getPercentages(a, b);
+      var percentages = getPercentages(a, b);
 
-       bg1.style.width = percentages.a + "%";
-       bg2.style.width = percentages.b + "%";
+      bg1.style.width = percentages.a + "%";
+      bg2.style.width = percentages.b + "%";
 
-       $scope.$apply(function () {
-         $scope.aPercent = percentages.a;
-         $scope.bPercent = percentages.b;
-         $scope.total = a + b;
-       });
+      $scope.$apply(function () {
+        $scope.aPercent = percentages.a;
+        $scope.bPercent = percentages.b;
+        $scope.total = a + b;
+      });
     });
   };
 
-  var init = function(){
-    document.body.style.opacity=1;
+  var init = function() {
+    document.body.style.opacity = 1;
     updateScores();
   };
-  socket.on('message',function(data){
+
+  socket.on('message', function(data) {
     init();
   });
 });
 
 function getPercentages(a, b) {
   var result = {};
-
   if (a + b > 0) {
     result.a = Math.round(a / (a + b) * 100);
     result.b = 100 - result.a;
   } else {
     result.a = result.b = 50;
   }
-
   return result;
 }
