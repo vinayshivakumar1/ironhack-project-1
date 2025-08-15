f06b69544bbdc6ae3491a28b86cdbc8fbe9b3141
professordiogodev <diogo@dionamite.com>
2025-08-15 12:08:54 +0100

    adding app.py fix and docker command additional build

--- vote/app.py
+++ vote/app.py
@@ -52,4 +52,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 80), debug=True, threaded=True)
